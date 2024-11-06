5a342c8261e3e25a52ecd2c4ed35a48e46c485fd
Christoph Heindl <christoph.heindl@gmail.com>
2024-11-06 20:29:28 +0100

    added selective copying task

--- selective_copying.py
+++ selective_copying.py
@@ -14,18 +14,18 @@ def torch_copying_data(
     This code is adopted from the copying.py script in the S4 repository. The original code can be found at:
     https://github.com/state-spaces/s4/blob/e757cef57d89e448c413de7325ed5601aceaac13/src/dataloaders/datasets/copying.py
 
-    Parameters:
-    L (int): Number of padding tokens
-    M (int): Number of tokens to memorize
-    A (int): Alphabet size
-    variable (bool): If True, selective copying task
-    variable_length (bool): If True, randomize number of tokens to memorize
-    batch_shape (tuple): Shape of the batch
-    one_hot (bool): If True, convert the input sequence into a one-hot encoded tensor
-    reverse (bool): If True, reverse the order of the target sequence
+    Params:
+        L (int): Number of padding tokens
+        M (int): Number of tokens to memorize
+        A (int): Alphabet size
+        variable (bool): If True, selective copying task
+        batch_shape (tuple): Shape of the batch
+        one_hot (bool): If True, convert the input sequence into a one-hot encoded tensor
+        reverse (bool): If True, reverse the order of the target sequence
 
     Returns:
-    tuple: Generated input sequence and target sequence
+        x: generated sequence
+        y: target sequence
     """
     tokens = torch.randint(low=1, high=A - 1, size=batch_shape + (M,))
     if variable:
@@ -104,19 +104,45 @@ def train(cfg: dict):
                 f'Step [{step+1}/{cfg["num_steps"]}], Loss: {step_loss/cfg["batch_size"]:.4f}, Accuracy: {accuracy:.2f}%'
             )
 
+    return model
+
+
+@torch.no_grad()
+def validate(cfg, model):
+    dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.eval()
+    inputs, targets = inputs, targets = torch_copying_data(
+        cfg["seq_len"],
+        cfg["num_memorize"],
+        cfg["vocab_size"],
+        variable=True,
+        batch_shape=(cfg["batch_size"],),
+        one_hot=False,
+        reverse=False,
+    )
+    inputs = inputs.to(dev)
+    targets = targets.to(dev)
+    outputs = model(inputs).permute(0, 2, 1)
+    total = targets.size(0) * targets.size(1)
+    correct = (outputs.argmax(1) == targets).sum().item()
+    accuracy = 100 * correct / total
+
+    print(f"Validation Accuracy: {accuracy:.2f}%")
+
 
 if __name__ == "__main__":
 
     cfg = {
-        "seq_len": 32,
+        "seq_len": 64,
         "num_memorize": 4,
-        "vocab_size": 4,
-        "emb_dims": 16,
-        "hidden_dims": 256,
+        "vocab_size": 6,
+        "emb_dims": 4,
+        "hidden_dims": 64,
         "num_layers": 3,
         "batch_size": 64,
-        "num_steps": 10000,
-        "lr": 1e-4,
+        "num_steps": 1500,
+        "lr": 1e-3,
     }
 
-    train(cfg)
+    model = train(cfg)
+    validate(cfg, model)
