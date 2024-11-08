b3882e58106eb9b5797604b40f777874ce37768c
Christoph Heindl <cheind@profactor.at>
2024-11-08 09:32:37 +0100

    adapted tests to new interface

--- tests/test_mingru.py
+++ tests/test_mingru.py
@@ -12,16 +12,16 @@ def test_mingru_basic():
     x = torch.randn(1, 5, 1)
 
     h = mingru.functional.g(h0)  # <- don't forget the activation
-    h_seq = []
+    out_seq = []
     for i in range(5):
-        h = rnn(x[:, i : i + 1], h, return_all_outputs=True)
+        out, h = rnn(x[:, i : i + 1], h)
         assert h.shape == (2, 1, 1, 5)
-        h_seq.append(h[-1])
-    h_seq = torch.cat(h_seq, 1)
+        out_seq.append(out)
+    out_seq = torch.cat(out_seq, 1)
 
-    h_par = rnn(x, mingru.functional.g(h0))
-    assert h_par.shape == (1, 5, 5)
-    assert torch.allclose(h_seq, h_par)
+    out_par, h = rnn(x, mingru.functional.g(h0))
+    assert out_par.shape == (1, 5, 5)
+    assert torch.allclose(out_seq, out_par)
 
 
 @pytest.mark.parametrize("num_layers", [1, 2, 3])
@@ -34,87 +34,62 @@ def test_mingru(num_layers):
 
     # sequential pattern
     h = mingru.functional.g(h0)
-    h_seq = []
+    out_seq = []
     for i in range(x.shape[1]):
         # For more than 1 layers we need all intermediate hidden states
         # for next invocation
-        h = rnn(x[:, i : i + 1], h, return_all_outputs=True)
+        out, h = rnn(x[:, i : i + 1], h)
         # However, we are usually interested in just the last one as output
-        h_seq.append(h[-1])
-    h_seq = torch.cat(h_seq, 1)
+        out_seq.append(out)
+    out_seq = torch.cat(out_seq, 1)
 
-    h_par = rnn(x, mingru.functional.g(h0))
-    assert h_par.shape == (2, 5, 5)
-    assert torch.allclose(h_seq, h_par)
-
-    h_par = rnn(x, mingru.functional.g(h0), return_all_outputs=True)
-    assert h_par.shape == (num_layers, 2, 5, 5)
-    assert torch.allclose(h_seq, h_par[-1])
+    out_par, h = rnn(x, mingru.functional.g(h0))
+    assert out_par.shape == (2, 5, 5)
+    assert h.shape == (num_layers, 2, 1, 5)
+    assert torch.allclose(out_seq, out_par)
 
 
-@pytest.mark.parametrize("num_layers", [1, 2, 3])
-def test_chucked_helper(num_layers):
-
-    rnn = mingru.MinGRU(input_dims=3, hidden_dims=5, num_layers=num_layers)
-
-    h0 = torch.zeros(2, 1, 5)
-    x = torch.randn(2, 5, 3)
-
-    # using sequential helper
-    h_seq = []
-    seqfn = rnn.create_chunked_helper(mingru.functional.g(h0))
-    for i in range(x.shape[1]):
-        h_seq.append(seqfn(x[:, i : i + 1]))
-    h_seq = torch.cat(h_seq, 1)
-
-    h_par = rnn(x, mingru.functional.g(h0))
-    assert torch.allclose(h_seq, h_par)
-
-
+@torch.no_grad()
 def test_interface():
 
     # Instantiate
-    rnn = mingru.MinGRU(input_dims=3, hidden_dims=64, num_layers=2)
+    B, I, H, L, S = 10, 3, 64, 2, 128
+    rnn = mingru.MinGRU(
+        input_dims=I,
+        hidden_dims=H,
+        num_layers=L,
+        dropout=0.0,
+        residual=True,
+    ).eval()
 
-    # Invoke without initial hidden state.
+    # Invoke for input x with sequence length 128 and batch-size 10
     # This will allocate 'zero' hidden state for each layer.
-    h = rnn(torch.randn(10, 128, 3))
-    assert h.shape == (10, 128, 64)
+    out, h = rnn(torch.randn(B, S, I))
+    assert out.shape == (B, S, H)
+    assert h.shape == (L, B, 1, H)
 
-    # Invoke without initial hidden state and get all intermediate hidden states
-    h = rnn(torch.randn(10, 128, 3), return_all_outputs=True)
-    assert h.shape == (2, 10, 128, 64)
+    # Invoke with initial/previous hidden state.
+    # Hidden state must be expandable to shape (L,B,1,H)
+    out, h = rnn(torch.randn(B, S, I), h=torch.ones(B, 1, H) * 0.5)
+    assert out.shape == (B, S, H)
+    assert h.shape == (L, B, 1, H)
 
-    # Invoke with initial hidden state
-    h = rnn(torch.randn(10, 128, 3), h=torch.ones(10, 1, 64) * 0.5)
-    assert h.shape == (10, 128, 64)
-
-    # For sequential/chunked iterative prediction use
-    data = torch.randn(10, 128, 3)
-    h0 = torch.ones(10, 1, 64) * 0.5
-    h_seq = []
-    pred = rnn.create_chunked_helper(h0)
-    for i in range(data.shape[1]):
-        h = pred(data[:, i : i + 1])
-        h_seq.append(h)
-    h_seq = torch.cat(h_seq, 1)
-    h_par = rnn(data, h0)
-    assert torch.allclose(h_seq, h_par, atol=1e-5)
-
-    # Or without the chunked helper
+    # Sequential prediction pattern
+    data = torch.randn(B, S, I)
+    h0 = torch.ones(L, B, 1, H) * 0.5
     h = h0
-    h_seq = []
+    out_seq = []
+    out_hs = []
     for i in range(data.shape[1]):
-        # For more than 1 layers we need all intermediate hidden states
-        # for next invocation
-        h = rnn(data[:, i : i + 1], h, return_all_outputs=True)
-        # However, we are usually interested in just the last one as output
-        h_seq.append(h[-1])
-    h_seq = torch.cat(h_seq, 1)
-    h_par = rnn(data, h0)
-    assert torch.allclose(h_seq, h_par, atol=1e-5)
+        out, h = rnn(data[:, i : i + 1], h=h)
+        out_hs.append(h)
+        out_seq.append(out)
+    out_seq = torch.cat(out_seq, 1)
+    assert out_seq.shape == (B, S, H)
 
-    # Note, don't use all-zeros for initial hidden state, instead
-    # use the activation function
-    h = rnn(torch.randn(10, 128, 3), mingru.functional.g(torch.zeros(10, 1, 64)))
-    assert h.shape == (10, 128, 64)
+    # Parallel prediction pattern
+    out_par, h = rnn(data, h0)
+    assert torch.allclose(out_seq, out_par, atol=1e-4)
+
+    # Note, don't use <= 0 for initial hidden state, instead
+    h0 = mingru.functional.g(torch.zeros(10, 1, 64))
