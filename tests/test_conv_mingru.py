435b905e5e86eb2ba15fe7091fe938f9c5296d5c
Christoph Heindl <christoph.heindl@gmail.com>
2024-11-18 15:56:04 +0100

    added test for sequential

--- tests/test_conv_mingru.py
+++ tests/test_conv_mingru.py
@@ -8,14 +8,19 @@ from mingru.functional import conv_mingru
 
 def test_conv_mingru_functional():
 
-    rnn = mingru.MinGRU(input_dims=1, hidden_dims=5, num_layers=2)
-
     x = torch.randn(1, 10, 2, 32, 32)
     h0 = torch.zeros(1, 1, 5, 32, 32)
-
-    h = mingru.functional.g(h0)  # <- don't forget the activation
 
     kernel = torch.randn(5 * 2, 2, 3, 3)
     bias = torch.randn(5 * 2)
 
-    conv_mingru(x, h, kernel, bias)
+    h = mingru.functional.g(h0)  # <- don't forget the activation
+
+    out_seq = []
+    for i in range(x.shape[1]):
+        # For more than 1 layers we need all intermediate hidden states
+        # for next invocation
+        h = conv_mingru(x[:, i : i + 1], h, kernel, bias)
+        print(h.shape)
+        out_seq.append(h)
+    out_seq = torch.cat(out_seq, 1)
