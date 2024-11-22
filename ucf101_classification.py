fcee5b1586a81b018ae5520b379e42c5322bd49a
Christoph Heindl <cheind@profactor.at>
2024-11-22 09:54:04 +0100

    aligned testing with orig-paper

--- ucf101_classification.py
+++ ucf101_classification.py
@@ -35,8 +35,6 @@ def get_train_transform():
     return v2.Compose(
         [
             ToVideo(),
-            v2.Resize((256, 256)),
-            v2.RandomRotation(10),
             v2.RandomResizedCrop(224),
             v2.RandomHorizontalFlip(),
             v2.ToDtype(torch.float, scale=True),
@@ -49,7 +47,7 @@ def get_test_transform():
     return v2.Compose(
         [
             ToVideo(),
-            v2.Resize((224, 224)),
+            v2.FiveCrop(224),
             v2.ToDtype(torch.float, scale=True),
             v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
@@ -60,8 +58,8 @@ def get_dataset(cfg, train: bool, transform: Callable):
     ds = UCF101(
         cfg["ucf101_path"],
         cfg["ucf101_annpath"],
-        frames_per_clip=15,
-        step_between_clips=15,
+        frames_per_clip=10,
+        step_between_clips=10,
         fold=cfg["ucf101_fold"],
         train=train,
         output_format="TCHW",
@@ -92,32 +90,33 @@ class UCF101Classifier(torch.nn.Module):
             kernel_sizes=3,
             strides=2,
             paddings=1,
-            dropout=0.25,
-            residual=True,
+            dropout=0.1,
+            residual=False,
             bias=True,
         )
 
-        self.conv_logits1 = torch.nn.Conv2d(cfg["hidden_sizes"][-1], 256, kernel_size=1)
-        self.conv_logits2 = torch.nn.Conv2d(256, 101, kernel_size=1)
+        self.fc1 = torch.nn.Linear(sum(cfg["hidden_sizes"]), 256)
+        self.bc1 = torch.nn.BatchNorm1d(256)
+        self.fc2 = torch.nn.Linear(256, 101)
 
     def forward(self, video):
         B, S = video.shape[:2]
         features = self.backbone(video.flatten(0, 1)).unflatten(0, (B, S))
         # (B,S,512,28,28)
-        out, _ = self.rnn.forward(features, h=None)
-        # (B,S,256,2,2) with strides=2, padding=1
-        out = self.conv_logits1(out[:, -1])
-        out = F.relu(out)
-        out = self.conv_logits2(out)
-        # (B,101,2,2)
-        return F.adaptive_max_pool2d(out, 1).squeeze(2, 3)
+        out, hs = self.rnn.forward(features, h=None)
+
+        hs = [F.adaptive_avg_pool2d(hh.squeeze(1), (1, 1)) for hh in hs]
+        h = torch.cat(hs, 1).squeeze()
+        y = self.fc1(h)
+        y = F.relu(self.bc1(y))
+        y = self.fc2(y)
+        return y
 
 
 def train(cfg):
 
     dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     classifier = UCF101Classifier(cfg).to(dev)
-    # v = torch.randn(16, 10, 3, 224, 224).to(dev)
 
     transform = get_train_transform()
     ds = get_dataset(cfg, train=True, transform=transform)
@@ -165,7 +164,7 @@ def train(cfg):
                     f"Step {step+1}, Loss: {loss:.4f}, Accuracy: {accuracy:.2f}%"
                 )  # NOQA
             if (step + 1) % 200 == 0:
-                val_acc = evaluate(classifier, dev, dl_val)
+                val_acc = validate(classifier, dev, dl_val)
                 print(f"Step {step+1}, Validation Accuracy: {val_acc*100:.2f}%")
                 if val_acc > best_acc:
                     scripted = torch.jit.script(classifier)
@@ -179,13 +178,13 @@ def train(cfg):
 
 
 @torch.no_grad()
-def evaluate(classifier: torch.nn.Module, dev: torch.device, dl):
+def validate(classifier: torch.nn.Module, dev: torch.device, dl):
     classifier.eval()
 
     total = 0
     total_correct = 0
 
-    for step, (video, labels) in enumerate(dl):
+    for video, labels in dl:
         video = video.to(dev)
         labels = labels.to(dev)
         logits = classifier(video)
@@ -200,23 +199,44 @@ def evaluate(classifier: torch.nn.Module, dev: torch.device, dl):
 
 @torch.no_grad()
 def test(cfg, classifier):
+    # From each of these selected sub-volumes, we obtain 10 inputs for our model, i.e. 4
+    # corners, 1 center, and their horizontal flipping. The final pre- diction score
+    # is obtained by averaging across the sampled sub-volumes and their cropped regions.
     dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     classifier.eval()
 
-    transform = get_test_transform()
-    ds = get_dataset(cfg, train=False, transform=transform)
-    indices = np.random.permutation(len(ds))
-    ds_test = torch.utils.data.Subset(ds, indices[:100])
-    dl_test = torch.utils.data.DataLoader(
-        ds_test,
-        batch_size=cfg["batch_size"],
-        shuffle=True,
-        num_workers=cfg["dl_workers"],
-        collate_fn=custom_collate,
-    )
+    ds = get_dataset(cfg, train=False, transform=None)
+    trans = get_test_transform()
 
-    test_acc = evaluate(classifier, dev, dl_test)
-    print(f"Test Accuracy: {test_acc*100:.2f}%")
+    total = 0
+    total_correct = 0
+
+    # For each video
+    labels = [ds.samples[i][1] for i in ds.indices]
+    n_videos = len(ds.video_clips.video_paths)
+    for vidx, label in zip(range(n_videos), labels):
+        clips = ds.video_clips.subset([vidx])
+        # randomly select up to 25 clips
+        cids = np.random.permutation(len(clips))[:25]
+        all_logits = []
+        for cidx in cids:
+            video, _, _, _ = clips.get_clip(cidx)
+
+            # Five-crops + flip
+            crops = trans(video)
+            crops = torch.stack(crops, 0)
+            hcrops = v2.functional.horizontal_flip(crops)
+            input = torch.cat((crops, hcrops), 0).to(dev)
+
+            logits = classifier(input)
+            all_logits.append(logits)
+        all_logits = torch.cat(all_logits, 0)
+        pred = all_logits.mean(0)
+        total_correct += pred.argmax(0).item() == label
+        total += 1
+        print(f"{vidx+1}/{n_videos}, acc {total_correct/total}")
+
+    print(f"acc {total_correct/total}")
 
 
 if __name__ == "__main__":
@@ -232,13 +252,13 @@ if __name__ == "__main__":
         "num_train_steps": 20000,
         "num_test_steps": 100,
         "batch_size": 16,
-        "lr": 1e-4,
+        "lr": 1e-3,
     }
     model = train(cfg)
 
     # # Script and eval the model
-    scripted = torch.jit.script(model)
-    torch.jit.save(scripted, "ucf101_classifier.pt")
+    # scripted = torch.jit.script(model)
+    # torch.jit.save(scripted, "ucf101_classifier.pt")
 
-    # scripted = torch.jit.load("ucf101_classifier.pt")
+    scripted = torch.jit.load("ucf101_classifier_best.pt")
     test(cfg, scripted)
